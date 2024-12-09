aaab496481e4b44c8c26da4dd55aa3304d7de79f
Alexander <sasha123890555@gmail.com>
2024-12-09 20:47:35 +0300

    started eee

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,12 +4,10 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
-  staticDirs: ['../public'],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
